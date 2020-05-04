b0f59a46249d8a993b5f4714e540d99c38fe649b
richardyang-gdc <richard.yang@perficient.com>
2020-05-04 23:20:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -13,6 +13,4 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
-git clone https://github.com/openwrt-develop/luci-app-koolproxy
-git clone https://github.com/openwrt-develop/luci-app-shadowsocks
 git clone https://github.com/SuLingGG/luci-app-ssr-plus.git
