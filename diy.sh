31e46ca28e7d937d9f3a1c2e3014296f1ecc33de
richardyang-gdc <richard.yang@perficient.com>
2020-05-04 19:52:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 #拦截广告的app
 
@@ -14,4 +14,4 @@ cd package
 mkdir openwrt-packages
 cd openwrt-packages
 git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git
-git clone https://github.com/Lienol/openwrt-package.git
+git clone https://github.com/maxlicheng/luci-app-ssr-plus.git
